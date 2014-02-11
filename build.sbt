fdf1d3ccec56748b30cc2e504e8fd7430f8ddf60
Francois Aubertin <francois.aubertin@gmail.com>
2014-02-11 21:08:04 +0000

    Implementing UI.

--- build.sbt
+++ build.sbt
@@ -6,6 +6,10 @@ version := "1.0"
 
 scalaVersion := "2.10.3"
 
+libraryDependencies += "org.scala-lang" % "scala-swing" % "2.10.3"
+
+libraryDependencies += "org.quartz-scheduler" % "quartz" % "2.2.1"
+
 assemblySettings
 
 mainClass in assembly := Some("timer.Main")
--- src/main/scala/timer/Main.scala
+++ src/main/scala/timer/Main.scala
@@ -1,10 +1,32 @@
 package timer
 
-object Main {
+import scala.swing.SimpleSwingApplication
+import scala.swing.MainFrame
+import scala.swing.Button
+import scala.swing.GridPanel
+import scala.swing.Label
+import scala.swing.TextField
+import java.awt.Dimension
 
-    def main(args: Array[String]) {
-        println("HEY")
+object Main extends SimpleSwingApplication {
+
+    def top = new MainFrame {
+        title = "Timer"
+        contents = new GridPanel(6, 4) {
+            vGap = 15
+            hGap = 15
+            contents ++= List(
+                    new Filler(), new Filler(), new Filler(), new Filler(),
+                    new Filler(), new Label("Cron expression"), new TextField(), new Filler(),
+                    new Filler(), new Label("Sound file"), new TextField(), new Filler(),
+                    new Filler(), new Label("Status"), new Label(), new Filler(),
+                    new Filler(), new Button("START"), new Button("STOP"), new Filler(),
+                    new Filler(), new Filler(), new Filler(), new Filler())
+        }
     }
 
 }
 
+class Filler extends Label {
+
+}
